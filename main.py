12eb3a7c95477911db4cd5f1484f00b0ea6dfdef
MaciejKubicki <kubicki.progamista@gmail.com>
2020-12-13 14:51:27 +0100

    add function name_of_realms

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from wowapi import WowApi
 
-list = []
+list_of_realms = []
 api = WowApi('0d7282069bad432c8cac95125523f7cf','wLcGwQgP72kzpMzH81I8siD4UbZTlH2f') #key and secretkey from blizzardapi
 #server_id = api.get_realm_index('eu', list)
 
-name_of_realms = api.get_realm_index('eu', 'dynamic-eu')
-api.get_realm('eu','dynamic-eu',)
-#api.get_account_profile_summary('eu','')
-#api.get_auctions('eu', 'silvermoon', locale='de_DE')
+name_of_realms_dict = api.get_realm_index('eu', 'dynamic-eu')
+
+def add_to_list_realms(name_of_realms):
+    for x in range(len(name_of_realms['realms'])):
+        list_of_realms.append(name_of_realms['realms'][x])
+
+add_to_list_realms(name_of_realms_dict)
+
+
